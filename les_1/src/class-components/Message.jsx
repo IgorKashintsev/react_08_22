09bd458906db6c92f37bd4621484125673051c88
IgorKashintsev <igorchello99@yandex.ru>
2022-09-09 13:47:09 +0300

    les_1 new

---
+++ les_1/src/class-components/Message.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import style from '../components/Message.module.css';
+
+export class Message extends React.Component {
+
+  render() {
+    return (
+      <div className="Message">
+        <p>Message-Class: { this.props.message}</p>
+        <p className={ style.Message }>New Message-Class: { this.props.newMessage }</p>
+        <input type="text" onChange={ this.props.handleChangeMessage }/>
+      </div>
+    );
+  }
+}
