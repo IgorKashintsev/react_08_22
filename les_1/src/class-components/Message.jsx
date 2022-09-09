4bc109032f63ad0caf70a1496c4c22001a2ece51
IgorKashintsev <igorchello99@yandex.ru>
2022-09-09 16:27:37 +0300

    Les_1 final commit

--- les_1/src/class-components/Message.jsx
+++ les_1/src/class-components/Message.jsx
@@ -6,9 +6,9 @@ export class Message extends React.Component {
   render() {
     return (
       <div className="Message">
-        <p>Message-Class: { this.props.message}</p>
-        <p className={ style.Message }>New Message-Class: { this.props.newMessage }</p>
-        <input type="text" onChange={ this.props.handleChangeMessage }/>
+        <p>Message-Class: {this.props.message}</p>
+        <p className={style.Message}>New Message-Class: {this.props.newMessage}</p>
+        <input type="text" onChange={this.props.changeMessage}/>
       </div>
     );
   }
